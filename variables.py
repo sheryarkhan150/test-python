dc2a2292988f95c9436201703617e83f92db5a09
Sheryar Khan <sheri.ali.khan@gmail.com>
2023-09-20 20:09:29 +0500

    Added .py files as i practised

---
+++ variables.py
@@ -0,0 +1,53 @@
+
+#First Comment
+
+#Variables
+"""
+x = 5
+y = 10
+z = "Sheri"
+print(x)
+print(y)
+print(z)
+"""
+#typeCasting
+
+x = float(3) 
+y = int(3)
+z = str(4)
+print(x)
+print(y)
+print(z)
+
+X = 6
+Y = 10
+Z = 'Ali'
+
+print(X)
+print(Y)
+print(Z)
+
+#Variable Names
+
+myName = "Sheryar"
+_myName = "Sheryar"
+my_Name = "Sheryar"
+myname = "Sheryar"
+MYNAME = "Sheryar"
+MYname = "Sheryar"
+myNAME = "Sheryar"
+
+#AssignMultiValues
+
+a,b,c = 'Peach', 'Green' , "Cherry"
+print(a)
+print(b)
+print(c)
+
+#Unpack a list
+
+fruits = ["apple","banana","mango"]
+f1,f2,f3 = fruits
+print(f1)
+print(f2)
+print(f3)
