57f111b24d499fd28603c7290cc161cee76f7388
Sheryar Khan <sheri.ali.khan@gmail.com>
2023-09-25 17:03:51 +0500

    If else

---
+++ If...else.py
@@ -0,0 +1,29 @@
+a = 10
+b = 20
+
+if b > a:
+    print("B is greater than a")
+
+
+c = 30
+d = 30
+if c > d:
+    print("C is greater than D")
+elif c == d:
+     print("C and D are equal")
+
+e = 20
+f = 30
+if e > f:
+    print("E is greater than F")
+elif e == f:
+     print("E and F are equal")
+else:
+    print("F is greater than E")
+
+
+a1 = 200
+a2 = 33
+a3 = 500
+if a1>a2 or a1>a3:
+    print("At least one of the condition is True")
