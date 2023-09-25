5f097cac7ea4c77cd6f98c38463cd64412658b0f
Sheryar Khan <sheri.ali.khan@gmail.com>
2023-09-25 17:11:06 +0500

    While-Loop

---
+++ WhileLoops.py
@@ -0,0 +1,18 @@
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+"""""
+j = 1
+while j < 6:
+    print(j)
+    if j == 3:
+        break
+    j += 1
+"""""
+k = 1
+while k < 6:
+    k += 1
+    if k == 3:
+        continue
+        print(k)
